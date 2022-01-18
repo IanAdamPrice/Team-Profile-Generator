1237d0077e0ea84680f0407d72b0edce055cb2a4
IanAdamPrice <ianadamprice@gmail.com>
2022-01-17 23:16:07 -0500

    fixed styling issues

--- src/generatePage.js
+++ src/generatePage.js
@@ -2,14 +2,14 @@ createManager = function (manager) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="card-header bg-primary text-light">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4>
+                <h4><i class="material-icons">free_breakfast</i>Manager</h4>
             </div>
-            <div class="card-body">
-]               <p class="id">ID: ${manager.id}</p>
-                <p class="email>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office">Office Number: ${manager.officeNum}</p>
+            <div class="card-body bg-light text-dark">
+                <p class="id bg-white p-2 border rounded m-0">ID: ${manager.id}</p>
+                <p class="email bg-white p-2 border rounded m-0">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office bg-white p-2 border rounded m-0">Office Number: ${manager.officeNum}</p>
             </div>
         </div>
     </div>
@@ -20,14 +20,14 @@ createEngineer = function (engineer) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="card-header bg-primary text-light">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4>
+                <h4><i class="material-icons">work</i>Engineer</h4>
             </div>
-            <div class="card-body">
-                <p class="id">ID: ${engineer.id}</p>
-                <p class="email>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-                <p class="github">GitHub:<a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            <div class="card-body bg-light text-dark">
+                <p class="id bg-white p-2 border rounded m-0">ID: ${engineer.id}</p>
+                <p class="email bg-white p-2 border rounded m-0">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github bg-white p-2 border rounded m-0">GitHub:<a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
             </div>
         </div>
     </div>
@@ -38,15 +38,15 @@ createIntern = function (intern) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="card-header bg-primary text-light">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4>
+                <h4><i class="material-icons">weekend</i>Intern</h4>
             </div>
 
-            <div class="card-body">
-                <p class="id">ID: ${intern.id}</p>
-                <p class="email>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
-                <p class="school">School: ${intern.school}</p>
+            <div class="card-body bg-light text-dark">
+                <p class="id bg-white p-2 border rounded m-0">ID: ${intern.id}</p>
+                <p class="email bg-white p-2 border rounded m-0">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school bg-white p-2 border rounded m-0">School: ${intern.school}</p>
             </div>
         </div>
     </div>
@@ -65,11 +65,12 @@ generatePage = (employeeCards) => {
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
               <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+              <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
               <link rel="stylesheet" href="./style.css">
           </head>
           <body>
               <header>
-                  <nav class="navbar" id="navbar">
+                  <nav class="navbar bg-danger text-white" id="navbar">
                       <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
                   </nav>
               </header>
@@ -77,7 +78,7 @@ generatePage = (employeeCards) => {
                   <div class="container">
                       <div class="row" id="team-cards">
                           <!--Generated Employee Cards-->
-                          ${employeeCards}
+                            ${employeeCards}
                       </div>
                   </div>
               </main>
